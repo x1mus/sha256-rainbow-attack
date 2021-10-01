99dc3dbfc66bc3d99297ae1cf0875a268240b3eb
Herrgrim0 <romain.grimau@ulb.be>
2021-10-01 11:36:04 +0200

    reduction function done

--- utils/gen-passwd.cpp
+++ utils/gen-passwd.cpp
@@ -57,30 +57,23 @@ void reduce_hash(std::string hash, int time_reduced, int passwd_length){
 inline void padding_reduction(std::string& new_password, int passwd_length) {
 	/*transform reduction into a string with a size of twice de password*/
 	std::string padding_chars= "0000000000000000";
-	int padding = std::strtoull(new_password.c_str(), nullptr, 10) % (2 * passwd_length); // number of 0 to add to our password
-	std::cout << "padding: " << padding << std::endl;
+	unsigned padding = (2 * passwd_length) - new_password.size(); // number of 0 to add to our password
 	new_password.insert(0, padding_chars, 0, padding);
 
 }
 
 inline std::string reduced_hash_to_new_password(std::string& new_password_as_digits){
-	std::cout << "in reduc_hash_to_n_passwd" << std::endl;
 	std::string new_password;
 	int pair_of_digits;
 	const char allowed_chars[] = "abcdefghijklmnopqrstuvwxyz"
 								 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 								 "0123456789"; // size 62
-	
-	std::cout << "on est là fraté" << std::endl;
+
 	for(int pos = 0; pos < new_password_as_digits.size(); pos+=2) {
-		std::cout << "for ce en nous" << std::endl;
 		pair_of_digits = std::stoi(new_password_as_digits.substr(pos, 2));
 		pair_of_digits %= 62;
 		new_password.push_back(allowed_chars[pair_of_digits]);
-		std::cout << "fonctionne" << std::endl;
 	}
-
-	std::cout << "in reduc_hash_to_n_passwd" << std::endl;
 
 	return new_password;
 }
