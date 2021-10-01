bab8760038bfb1f9160f0dd5839def0a3597bddd
Herrgrim0 <romain.grimau@ulb.be>
2021-10-01 11:12:23 +0200

    [enh] reduction function done. little debugging left to do

--- utils/gen-passwd.cpp
+++ utils/gen-passwd.cpp
@@ -4,6 +4,8 @@
 #include "passwd-utils.hpp"
 
 void reduce_hash(std::string, int, int);
+inline void padding_reduction(std::string&, int);
+inline std::string reduced_hash_to_new_password(std::string&);
 
 int main(int argc, char *argv[])
 {
@@ -32,7 +34,7 @@ int main(int argc, char *argv[])
 
 
 void reduce_hash(std::string hash, int time_reduced, int passwd_length){
-	std::string new_password;
+	std::string new_password = "";
 	unsigned long long int reduction;
 	double number_of_passwd = pow(56.0, static_cast<double>(passwd_length));
 
@@ -41,19 +43,44 @@ void reduce_hash(std::string hash, int time_reduced, int passwd_length){
 	reduction = std::strtoull(hash.c_str(), nullptr, 16);
 	reduction += time_reduced;
 	reduction = reduction % static_cast<unsigned long long int>(number_of_passwd);
+	new_password.append(std::to_string(reduction));
 
-	new_password = padding_reduction(reduction, passwd_length);
-
+	padding_reduction(new_password, passwd_length);
+	std::cout << new_password << std::endl;
+	new_password = reduced_hash_to_new_password(new_password);
+	std::cout << new_password << std::endl;
+	
 
 	//return reduction;
 }
 
-inline std::string padding_reduction(unsigned long long int reduction, int passwd_length) {
+inline void padding_reduction(std::string& new_password, int passwd_length) {
 	/*transform reduction into a string with a size of twice de password*/
-	std::string new_password = std::to_string(reduction);
 	std::string padding_chars= "0000000000000000";
-	int padding = reduction % (2 * passwd_length); // number of 0 to add to our password
+	int padding = std::strtoull(new_password.c_str(), nullptr, 10) % (2 * passwd_length); // number of 0 to add to our password
+	std::cout << "padding: " << padding << std::endl;
 	new_password.insert(0, padding_chars, 0, padding);
+
+}
+
+inline std::string reduced_hash_to_new_password(std::string& new_password_as_digits){
+	std::cout << "in reduc_hash_to_n_passwd" << std::endl;
+	std::string new_password;
+	int pair_of_digits;
+	const char allowed_chars[] = "abcdefghijklmnopqrstuvwxyz"
+								 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+								 "0123456789"; // size 62
+	
+	std::cout << "on est là fraté" << std::endl;
+	for(int pos = 0; pos < new_password_as_digits.size(); pos+=2) {
+		std::cout << "for ce en nous" << std::endl;
+		pair_of_digits = std::stoi(new_password_as_digits.substr(pos, 2));
+		pair_of_digits %= 62;
+		new_password.push_back(allowed_chars[pair_of_digits]);
+		std::cout << "fonctionne" << std::endl;
+	}
+
+	std::cout << "in reduc_hash_to_n_passwd" << std::endl;
 
 	return new_password;
 }
