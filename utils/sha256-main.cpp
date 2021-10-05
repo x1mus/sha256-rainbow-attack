e4df7593aa6f9bcf84fddf7965604e0f82138d9e
Herrgrim0 <romain.grimau@ulb.be>
2021-10-05 18:35:05 +0200

    [mod] for new sha256 object

--- utils/sha256-main.cpp
+++ utils/sha256-main.cpp
@@ -7,7 +7,8 @@ using std::endl;
  
 int main(int argc, char *argv[])
 {
-    string input = "grape";
+    SHA256 sha256;
+    const string input = "grape";
     string output1 = sha256(input);
  
     cout << "sha256('"<< input << "'):" << output1 << endl;
