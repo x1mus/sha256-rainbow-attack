7db47b1bb5c7413a7a8a237d2a7cab1900145a3a
Herrgrim0 <romain.grimau@ulb.be>
2021-10-06 23:03:58 +0200

    resolve surpadding problem

--- utils/sha256-main.cpp
+++ utils/sha256-main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include "sha256.h"
 #include "reduction.hpp"
  
@@ -12,21 +13,21 @@ int main(int argc, char *argv[])
     SHA256 sha256;
     string last_red = "grape214";
     string output1 = sha256(last_red);
+    unsigned password_length = 0;
     std::ifstream passwd_table("psswd.txt");
 	std::ofstream RainbowTable("RT.csv");
     
     cout << "sha256('"<< last_red << "'):" << output1 << endl;
-    std::cout << reduce_hash(output1, 0, output1.length());
+    std::cout << reduce_hash(output1, 0, output1.length() % 8);
 
     while (getline(passwd_table, last_red)){
-        std::cout << last_red << std::endl;
 		RainbowTable << last_red;
 		RainbowTable << ';';
+        password_length = strlen(last_red.c_str());
+
         for (unsigned i = 0; i < 50000; ++i) {
             output1 = sha256(last_red);
-            //std::cout << "output1: " << output1 << std::endl;
-            last_red = reduce_hash(output1, i, (int)output1.length()); //reduction
-            //std::cout << "last_red: " << last_red << std::endl;
+            last_red = reduce_hash(output1, i, password_length); //reduction
         }
         RainbowTable << last_red;
         RainbowTable << std::endl;
