6b95ed500ee7b5f60cd093e93db2d8c95683898b
Herrgrim0 <romain.grimau@ulb.be>
2021-10-06 22:23:20 +0200

    prototyping file

--- utils/sha256-main.cpp
+++ utils/sha256-main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include "sha256.h"
+#include "reduction.hpp"
  
 using std::string;
 using std::cout;
@@ -8,9 +10,27 @@ using std::endl;
 int main(int argc, char *argv[])
 {
     SHA256 sha256;
-    const string input = "grape";
-    string output1 = sha256(input);
- 
-    cout << "sha256('"<< input << "'):" << output1 << endl;
+    string last_red = "grape214";
+    string output1 = sha256(last_red);
+    std::ifstream passwd_table("psswd.txt");
+	std::ofstream RainbowTable("RT.csv");
+    
+    cout << "sha256('"<< last_red << "'):" << output1 << endl;
+    std::cout << reduce_hash(output1, 0, output1.length());
+
+    while (getline(passwd_table, last_red)){
+        std::cout << last_red << std::endl;
+		RainbowTable << last_red;
+		RainbowTable << ';';
+        for (unsigned i = 0; i < 50000; ++i) {
+            output1 = sha256(last_red);
+            //std::cout << "output1: " << output1 << std::endl;
+            last_red = reduce_hash(output1, i, (int)output1.length()); //reduction
+            //std::cout << "last_red: " << last_red << std::endl;
+        }
+        RainbowTable << last_red;
+        RainbowTable << std::endl;
+    }
+
     return 0;
 }
